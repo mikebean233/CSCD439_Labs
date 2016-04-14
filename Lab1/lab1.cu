0645217c3d5fe545c511fd28f908c8e4aab93ef9
Michael Peterson <mikebean233@yahoo.com>
2016-04-14 14:53:25 -0700

    first (naive) solution for lab 1

--- Lab1/lab1.cu
+++ Lab1/lab1.cu
@@ -1,5 +1,84 @@
 #include<stdio.h>
 
+__device__ void isPrime(int* d_values, int N){
+	int thisValue = blockIdx.x * blockDim.x + threadIdx.x;
+
+	if(thisValue == 0)
+		return false;
+
+	if(thisValue == 1)
+		return false;
+
+	if(thisValue == 2)
+		return true;
+
+	if(thisValue % 2 == 0)
+		return false;
+
+	int j = 3;
+	for(; j*j < N; j += 2){
+		if(j % thisValue == 0)
+			return false;
+	}
+
+	return true;
+}
+
+int main(int argc, char** argv){
+	if(argc != 3)
+		usage(1, argv[0]);
+
+	int N = atoi(argv[1]);
+	int BlockSize |= atoi(argv[2]);
+
+	if(!(n | BlockSize))
+		usage(2, argv[0]);
+
+	int arraySizeInBytes = sizeof(long long int) * N;
+
+	// allocate our arrays
+	long long int* h_array;
+	long long int* d_array;
+	h_array = (long long int*) malloc(arraySizeInBytes);
+	cudaMalloc(&d_array, arraySizeInBytes);
+
+	// caculate the grid size
+	int gridSize;
+	if(N % blockSize == 0)
+		gridSize = N / blockSize;
+	else
+		gridSize = (N / blockSize) + 1;
+
+
+	// run the kernel
+	doubleIndexValue<<<gridSize, blockSize>>>(d_array, N);
+
+	// copy the results back to the host array
+	cudaMemcpy(h_array, d_array, arraySizeInBytes, cudaMemcpyDeviceToHost);
+
+	// release the device array
+	cudaFree(d_array);
+
+	// Print our results
+	int i = 0;
+	for(; i < N; ++i){
+		printf("h_array[%d] = %d\n", i, h_array[i]);
+	}
+
+	// free the host memory
+	free(h_array);
+
+	return 0;
+
+
+}
+
+void usage(int exitStatus, char* progamName){
+	fprintf(stderr, "usage: %s N blockSize", programName);
+	exit(exitStatus);
+}
+
+/*
 // Gpu kernel code
 __global__ void doubleIndexValue(int* a, int n){
 	int threadId = blockIdx.x*blockDim.x + threadIdx.x;
@@ -51,7 +130,7 @@ int main(){
 	return 0;
 }
 
-
+*/
 
 
 
