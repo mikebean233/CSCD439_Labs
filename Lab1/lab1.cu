9ed83bdbefbc2bd07d7f433a7d9f5e88bacc7224
Michael Peterson <mikebean233@yahoo.com>
2016-04-14 15:51:07 -0700

    no message

--- Lab1/lab1.cu
+++ Lab1/lab1.cu
@@ -26,7 +26,7 @@ __global__ void isPrime(long long int* d_array, int N){
     }
 
     int j = 3;
-    for(; j*j < N; j += 2){
+    for(; j < N; j ++j){
         if(thisValue % j== 0){
             d_array[thisValue] = 0;
             return;
