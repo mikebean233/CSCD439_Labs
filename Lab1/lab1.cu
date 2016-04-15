14bc650c18a2372a4afba28c8cb7e1ec23817c90
Michael Peterson <mikebean233@yahoo.com>
2016-04-14 17:31:45 -0700

    no message

--- Lab1/lab1.cu
+++ Lab1/lab1.cu
@@ -1,19 +1,14 @@
 #include<stdio.h>
 
 void usage(int exitStatus, char* programName);
+long long int sumArray(int* array);
 
-__global__ void isPrime(long long int* d_array, int N){
-        int thisValue = blockIdx.x * blockDim.x + threadIdx.x;
+__global__ void isPrime(int* d_array, long long int N){
+    long long int theadId = blockIdx.x * blockDim.x + threadIdx.x;
+    long long ing thisValue = (threadId * 2) + 1;
 
-    if(thisValue == 0){
-        d_array[thisValue] = 0;
-                return;
-    }
-
-    if(thisValue == 1){
-        d_array[thisValue] = 0;
-            return;
-        }
+    if(thisValue < 1)
+    	return;
 
     if(thisValue == 2){
         d_array[thisValue] = 1;
@@ -25,8 +20,8 @@ __global__ void isPrime(long long int* d_array, int N){
         return;
     }
 
-    int j;
-    for(j = 3; j < thisValue; j += 2){
+    long long int j;
+    for(j = 3; j*j < thisValue; j += 2){
         if(thisValue % j== 0){
             d_array[thisValue] = 0;
             return;
@@ -40,26 +35,25 @@ int main(int argc, char** argv){
 	if(argc != 3)
 		usage(1, argv[0]);
 
-	int N = atoi(argv[1]);
+	int N = (long long int) atoi(argv[1]);
 	int blockSize = atoi(argv[2]);
 
 	if(!(N | blockSize))
 		usage(2, argv[0]);
 
-	int arraySizeInBytes = sizeof(long long int) * N;
+	int arraySizeInBytes = sizeof(long long int) * (N + 1);
 
 	// allocate our arrays
-	long long int* h_array;
-	long long int* d_array;
+	int* h_array;
+	int* d_array;
 	h_array = (long long int*) malloc(arraySizeInBytes);
 	cudaMalloc(&d_array, arraySizeInBytes);
 
+	// zero the memory in cuda
+	cudaMemset(d_array, 0, arraySizeInBytes);
+
 	// caculate the grid size
-	int gridSize;
-	if(N % blockSize == 0)
-		gridSize = N / blockSize;
-	else
-		gridSize = (N / blockSize) + 1;
+	int gridSize ((N + 1) / 2.0 / blockSize);
 
 
 	// run the kernel
@@ -81,6 +75,16 @@ int main(int argc, char** argv){
 	free(h_array);
 
 	return 0;
+}
+
+
+long long int sumArray(int* array, long long int arraySize){
+	long long int sum = 0;
+	long long index = 0;
+	for(; index < arraySize; ++index){
+		sum += array[index];
+	}
+	return sum;
 }
 
 void usage(int exitStatus, char* programName){
