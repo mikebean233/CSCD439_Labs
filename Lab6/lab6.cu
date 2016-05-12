9f217d2deb8b524d51e5b68ac6bcd8bf21290af9
Michael Peterson <mikebean233@yahoo.com>
2016-05-12 14:31:39 -0700

    Started Lab 7 / Lab 6

---
+++ Lab6/lab6.cu
@@ -0,0 +1,106 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <string.h>
+
+#define R 4
+#define C 40 
+
+/*
+ * It returns the length of a string pointed by pointer s,
+ * It acts like the cpu strlen() function
+ */
+__device__ int gpu_strlen(char * s)
+{
+}
+
+/*
+ * It returns 0 if input character ch is NOT an alphabetical letter
+ * Otherwise, it returns one.
+ */
+__device__ int gpu_isAlpha(char ch)
+{
+}
+
+/* Cuda kernel to count number of words in each line of text pointed by a.
+ * The output is stored back in 'out' array.
+ * numLine specifies the num of lines in a, maxLineLen specifies the maximal
+ * num of characters in one line of text.
+ */
+__global__ void wordCount( char **a, int **out, int numLine, int maxLineLen )
+{
+}  
+
+/* Print out the all lines of text in a on stdout
+ */ 
+void printArr( char **a, int lines )
+{
+    int i;
+    for(i=0; i<lines; i++)
+    {
+        printf("%s\n", a[i]);
+    }
+}
+
+
+int main()
+{
+    int i; 
+    char **d_in, **h_in, **h_out;
+    int h_count_in[R][C], **h_count_out, **d_count_in;
+
+    //allocate
+    h_in = (char **)malloc(R * sizeof(char *));
+    h_out = (char **)malloc(R * sizeof(char *));
+    h_count_out = (int **)malloc(R * sizeof(int *));
+
+    cudaMalloc((void ***)&d_in, sizeof(char *) * R);
+    cudaMalloc((void ***)&d_count_in, sizeof(int *) * R);
+
+    //alocate for string data
+    for(i = 0; i < R; ++i) 
+    {
+        cudaMalloc((void **) &h_out[i],C * sizeof(char));
+        h_in[i]=(char *)calloc(C, sizeof(char));//allocate or connect the input data to it
+        strcpy(h_in[i], "good morning and I'm a good student!");
+        cudaMemcpy(h_out[i], h_in[i], strlen(h_in[i]) + 1, cudaMemcpyHostToDevice);
+    }
+    cudaMemcpy(d_in, h_out, sizeof(char *) * R,cudaMemcpyHostToDevice);
+
+    //alocate for output occurrence
+    for(i = 0; i < R; ++i) 
+    {
+        cudaMalloc((void **) &h_count_out[i], C * sizeof(int));
+        cudaMemset(h_count_out[i], 0, C * sizeof(int));
+    }
+    cudaMemcpy(d_count_in, h_count_out, sizeof(int *) * R,cudaMemcpyHostToDevice);
+
+    printArr(h_in, R);
+    printf("\n\n");
+     
+    //set up kernel configuartion variables
+    dim3 grid, block;
+    block.x = 2;
+    block.y = 2;
+    grid.x  = ceil((float)C / block.x);
+    grid.y  = ceil((float)R / block.y); //careful must be type cast into float, otherwise, integer division used
+    //printf("grid.x = %d, grid.y=%d\n", grid.x, grid.y );
+
+    //launch kernel
+    wordCount<<<grid, block>>>( d_in, d_count_in, R, C);
+
+    //copy data back from device to host
+    for(i = 0; i < R; ++i) {
+        cudaMemcpy(h_count_in[i], h_count_out[i], sizeof(int) * C,cudaMemcpyDeviceToHost);
+    }
+    printf("Occurrence array obtained from device:\n");
+
+    for(i = 0; i < R; i ++) {
+        for(int j = 0; j < C; j ++)
+            printf("%4d", h_count_in[i][j]);
+        printf("\n");
+    }
+ 
+    return 0;
+}
+
---
+++ Lab7/diff.cu
@@ -0,0 +1,88 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+#define BLOCKSIZE 4 // Number of threads in each thread block
+ 
+// CUDA kernel. Each thread takes care of one element of a 
+__global__ void diffKernel( float *in, float *out, int n )
+{
+    // Wrtie the kernel to implement the diff operation on an array Using shared memory
+    extern __shared__ int s_data[];
+    
+    unsigned int global_index = blockDim.x * blockIdx.x + threadIdx.x;
+
+    // Make sure this thread is within bounds
+    if(global_index >= n - 2)
+        return;
+    
+    // Copy global data to shared memory
+    s_data[threadIdx.x] = in[global_index];
+
+    if(threadIdx.x == blockDim.x - 1)
+        s_data[threadIdx.x] in[global_index];
+
+    // Wait for all of the threads to reach the barrier
+    __syncthreads();
+
+    // Perform the actuall diff
+    out[global_index] = s_data[threadIdx.x + 1] - s_data[threadIdx.x];
+}  
+ 
+int main( int argc, char* argv[] )
+{
+    // Size of vectors
+    int i;
+    float input[] = {4, 5, 6, 7, 19, 10, 0, 4, 2, 3, 1, 7, 9, 11, 45, 23, 99, 29};
+    int n = sizeof(input) / sizeof(float); //careful, this usage only works with statically allocated arrays, NOT dynamic arrays
+
+    // Host input vectors
+    float *h_in = input;
+    //Host output vector
+    float *h_out = (float *) malloc(n * sizeof(float));
+ 
+    // Device input vectors
+    float *d_in;;
+    //Device output vector
+    float *d_out;
+ 
+    // Size, in bytes, of each vector
+    size_t bytes = n * sizeof(float);
+ 
+    // Allocate memory for each vector on GPU
+    cudaMalloc(&d_in, bytes);
+    cudaMalloc(&d_out, bytes);
+ 
+    // Copy host data to device
+    cudaMemcpy( d_in, h_in, bytes, cudaMemcpyHostToDevice);
+
+    // TODO: setup the blocksize and gridsize and launch the kernel below.
+    // Number of threads in each thread block
+     int blocksize = 5;
+    // Number of thread blocks in grid
+    int gridsize = ceil((float) n / blocksize);
+    // Execute the kernel
+    <<<blocksize, gridsize, blocksize + 1>>> diffKernel(d_in, d_out, n); 
+ 
+    // Copy array back to host
+    cudaMemcpy( h_out, d_out, bytes, cudaMemcpyDeviceToHost );
+ 
+    // Show the result
+    printf("The original array is: ");
+    for(i = 0; i < n; i ++)
+        printf("%4.0f,", h_in[i] );    
+    
+    printf("\n\nThe diff     array is: ");
+    for(i = 0; i < n; i++)
+        printf("%4.0f,", h_out[i] );    
+    puts("");
+    
+    // Release device memory
+    cudaFree(d_in);
+    cudaFree(d_out);
+ 
+    // Release host memory
+    free(h_out);
+ 
+    return 0;
+}
